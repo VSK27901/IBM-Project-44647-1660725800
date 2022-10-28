ea5ec875cff29481368b9eed3cd5610574c7e828
Anju Priya V <anjupriyachennai@gmail.com>
2022-10-28 15:29:02 +0530

    Made changes in Front end

--- Project_Development_Phase/Sprint_2/Flask/app.py
+++ Project_Development_Phase/Sprint_2/Flask/app.py
@@ -1,18 +1,135 @@
+from distutils.log import error
+from functools import wraps
+from http.client import HTTPException
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template,session, url_for,redirect,flash
 import pickle
 import inputScript
-
+import pymongo
+from passlib.hash import  pbkdf2_sha256
+import json
+  
 app = Flask(__name__,template_folder='../Flask')
 model = pickle.load(open('../Flask/Phishing_Website.pkl','rb'))
 
-@app.route('/')
-def home():
-    return render_template('./index.html')
+mongoDB=pymongo.MongoClient('mongodb+srv://IBM_Course:123456_*@cluster0.aghcl0q.mongodb.net/?retryWrites=true&w=majority')
+db=mongoDB['Web_Phishing_Detection']
+account=db.account
+app.secret_key=b'\x0b\xa9\x97\x07\xd8\xb3\xc3W\x8d\xeb\xf8\xe6\xbe\x92tb'
 
-@app.route('/predict')
+carouselDataFile = open('./static/json/carouselData.json')
+carouselData = json.load(carouselDataFile)
+aboutDataFile = open('./static/json/aboutData.json')
+aboutData = json.load(aboutDataFile)
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if('logged_in' in session):
+            return f(*args, **kwargs)
+        else:
+            return redirect('/')
+    return wrap
+
+
+def start_session(userInfo):
+    if userInfo:
+        userInfo['_id']=str(userInfo['_id'])
+    else:
+        raise HTTPException(status_code=404, detail=f"Unable to retrieve record")
+    del userInfo['password']
+    session['logged_in']=True
+    session['user']=userInfo
+    return redirect(url_for('index'))
+
+
+@app.route('/login/',methods=['POST'])
+def login():
+    if request.method=="POST":
+        email=request.form.get("email")
+        password=request.form.get("password")
+        if(account.find_one({"email":email})):
+            user=account.find_one({"email":email})
+            if(user and pbkdf2_sha256.verify(password,user['password'])):
+                return start_session(user)
+            else:
+                flash("Password is incorrect","loginError")
+                return redirect(url_for('index',loginError=True))
+        flash("Sorry, user with this email id does not exist","loginError")
+        return redirect(url_for('index',loginError=True))
+
+
+@app.route('/signup/',methods=['POST'])
+def signup():
+    if request.method=="POST":
+        userInfo={
+        "fullName":request.form.get('fullName'),
+        "email":request.form.get('email'),
+        "phoneNumber":request.form.get('phoneNumber'),
+        "password":request.form.get('password'),
+        }
+        userInfo['password']=pbkdf2_sha256.encrypt(userInfo['password'])
+        if(account.find_one({"email":userInfo['email']})):
+            flash("Sorry,user with this email already exist","signupError")
+            return redirect(url_for('index',signupError=True))
+        if(account.insert_one(userInfo)):
+            return start_session(userInfo)     
+    flash("Signup failed","signupError")
+    return redirect(url_for('index',signupError=True))
+
+
+@app.route('/logout/',methods=["GET"])
+def logout():
+    if request.method=="GET":
+        session.clear()
+    return redirect(url_for('index'))
+@app.route('/')
+def index():
+   
+    if(session and '_flashes' in dict(session)):
+        loginError=request.args.get('loginError')
+        signupError=request.args.get('signupError')
+        if(loginError):
+            return render_template('./index.html',loginError=loginError,carousel_content=carouselData['carousel_content'])
+        if(signupError):
+            return render_template('./index.html',signupError=signupError,carousel_content=carouselData['carousel_content'])
+    if(session and '_flashes' not in dict(session)):
+        print(dict(session))
+        if(session['logged_in']==True):
+            return render_template('./index.html',userInfo=session['user'],carousel_content=carouselData['carousel_content'])
+        else:
+            return render_template('./index.html',carousel_content=carouselData['carousel_content'])
+    else:
+        return render_template('./index.html',carousel_content=carouselData['carousel_content'])
+
+
+
+@app.route('/predict/')
+@login_required
 def predict():
-    return render_template('./templates/predict-form.html')
+    return render_template('./templates/predict-form.html',userInfo=session['user'])
+@app.route('/about/')
+def about():
+    if(session and session['logged_in']):
+        if(session['logged_in']==True):
+            return render_template('./templates/about.html',userInfo=session['user'],aboutContents=aboutData['aboutContents'])
+        else:
+            return render_template('./templates/about.html',aboutContents=aboutData['aboutContents'])
+    else:
+        return render_template('./templates/about.html',aboutContents=aboutData['aboutContents'])
+
+
+
+@app.route('/contact/')
+def contact():
+        if(session and session['logged_in']):
+            if(session['logged_in']==True):
+                return render_template('./templates/contact.html',userInfo=session['user'])
+            else:
+                return render_template('./templates/contact.html')
+        else:
+            return render_template('./templates/contact.html')
+
 
 ans = ""   
 bns = ""   
@@ -25,22 +142,25 @@ def y_predict():
     output=prediction[0]
     if(output==1):
         pred="You are safe!!  This is a legitimate Website."
-        return render_template('Final.html',bns=pred)
-    
+        return render_template('./templates/predict-form.html',bns=pred)
     else:
         pred="You are on the wrong site. Be cautious!"        
-        return render_template('Final.html',ans=pred)
+        return render_template('./templates/predict-form.html',ans=pred)
+
 
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
-    
     data = request.get_json(force=True)
     prediction = model.y_predict([np.array(list(data.values()))])
-
     output=prediction[0]
     return jsonify(output)        
  
+
+
+
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
     
--- Project_Development_Phase/Sprint_2/Flask/static/js/predict-form.js
+++ Project_Development_Phase/Sprint_2/Flask/static/js/predict-form.js
@@ -1,266 +1,107 @@
-$(function() {
-    var FADE_TIME = 150; // ms
-    var TYPING_TIMER_LENGTH = 400; // ms
-    var COLORS = [
-      '#e21400', '#91580f', '#f8a700', '#f78b00',
-      '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-      '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-    ];
-  
-    // Initialize varibles
-    var $window = $(window);
-    var $usernameInput = $('.usernameInput'); // Input for username
-    var $messages = $('.messages'); // Messages area
-    var $inputMessage = $('.inputMessage'); // Input message input box
-  
-    var $loginPage = $('.login.page'); // The login page
-    var $chatPage = $('.chat.page'); // The chatroom page
-  
-    // Prompt for setting a username
-    var username;
-    var connected = false;
-    var typing = false;
-    var lastTypingTime;
-    var $currentInput = $usernameInput.focus();
-  
-    var socket = io('http://chat.socket.io/');
-  
-    function addParticipantsMessage (data) {
-      var message = '';
-      if (data.numUsers === 1) {
-        message += "there's 1 participant";
-      } else {
-        message += "there are " + data.numUsers + " participants";
+$(document).ready(function () {
+  var current_fs, next_fs, previous_fs;
+  var opacity;
+  var current = 1;
+  var steps = $("fieldset").length;
+  setProgressBar(current);
+  function progessBarFunction() {
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now) {
+          opacity = 1 - now;
+          current_fs.css({
+            display: "none",
+            position: "relative",
+          });
+          next_fs.css({ opacity: opacity });
+        },
+        duration: 500,
       }
-      log(message);
-    }
-  
-    // Sets the client's username
-    function setUsername () {
-      username = cleanInput($usernameInput.val().trim());
-  
-      // If the username is valid
-      if (username) {
-        $loginPage.fadeOut();
-        $chatPage.show();
-        $loginPage.off('click');
-        $currentInput = $inputMessage.focus();
-  
-        // Tell the server your username
-        socket.emit('add user', username);
-      }
-    }
-  
-    // Sends a chat message
-    function sendMessage () {
-      var message = $inputMessage.val();
-      // Prevent markup from being injected into the message
-      message = cleanInput(message);
-      // if there is a non-empty message and a socket connection
-      if (message && connected) {
-        $inputMessage.val('');
-        addChatMessage({
-          username: username,
-          message: message
-        });
-        // tell server to execute 'new message' and send along one parameter
-        socket.emit('new message', message);
-      }
-    }
-  
-    // Log a message
-    function log (message, options) {
-      var $el = $('<li>').addClass('log').text(message);
-      addMessageElement($el, options);
-    }
-  
-    // Adds the visual chat message to the message list
-    function addChatMessage (data, options) {
-      // Don't fade the message in if there is an 'X was typing'
-      var $typingMessages = getTypingMessages(data);
-      options = options || {};
-      if ($typingMessages.length !== 0) {
-        options.fade = false;
-        $typingMessages.remove();
-      }
-  
-      var $usernameDiv = $('<span class="username"/>')
-        .text(data.username)
-        .css('color', getUsernameColor(data.username));
-      var $messageBodyDiv = $('<span class="messageBody">')
-        .text(data.message);
-  
-      var typingClass = data.typing ? 'typing' : '';
-      var $messageDiv = $('<li class="message"/>')
-        .data('username', data.username)
-        .addClass(typingClass)
-        .append($usernameDiv, $messageBodyDiv);
-  
-      addMessageElement($messageDiv, options);
-    }
-  
-    // Adds the visual chat typing message
-    function addChatTyping (data) {
-      data.typing = true;
-      data.message = 'is typing';
-      addChatMessage(data);
-    }
-  
-    // Removes the visual chat typing message
-    function removeChatTyping (data) {
-      getTypingMessages(data).fadeOut(function () {
-        $(this).remove();
-      });
-    }
-  
-    // Adds a message element to the messages and scrolls to the bottom
-    // el - The element to add as a message
-    // options.fade - If the element should fade-in (default = true)
-    // options.prepend - If the element should prepend
-    //   all other messages (default = false)
-    function addMessageElement (el, options) {
-      var $el = $(el);
-  
-      // Setup default options
-      if (!options) {
-        options = {};
-      }
-      if (typeof options.fade === 'undefined') {
-        options.fade = true;
-      }
-      if (typeof options.prepend === 'undefined') {
-        options.prepend = false;
-      }
-  
-      // Apply options
-      if (options.fade) {
-        $el.hide().fadeIn(FADE_TIME);
-      }
-      if (options.prepend) {
-        $messages.prepend($el);
-      } else {
-        $messages.append($el);
-      }
-      $messages[0].scrollTop = $messages[0].scrollHeight;
-    }
-  
-    // Prevents input from having injected markup
-    function cleanInput (input) {
-      return $('<div/>').text(input).text();
-    }
-  
-    // Updates the typing event
-    function updateTyping () {
-      if (connected) {
-        if (!typing) {
-          typing = true;
-          socket.emit('typing');
-        }
-        lastTypingTime = (new Date()).getTime();
-  
-        setTimeout(function () {
-          var typingTimer = (new Date()).getTime();
-          var timeDiff = typingTimer - lastTypingTime;
-          if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
-            socket.emit('stop typing');
-            typing = false;
-          }
-        }, TYPING_TIMER_LENGTH);
-      }
-    }
-  
-    // Gets the 'X is typing' messages of a user
-    function getTypingMessages (data) {
-      return $('.typing.message').filter(function (i) {
-        return $(this).data('username') === data.username;
-      });
-    }
-  
-    // Gets the color of a username through our hash function
-    function getUsernameColor (username) {
-      // Compute hash code
-      var hash = 7;
-      for (var i = 0; i < username.length; i++) {
-         hash = username.charCodeAt(i) + (hash << 5) - hash;
-      }
-      // Calculate color
-      var index = Math.abs(hash % COLORS.length);
-      return COLORS[index];
-    }
-  
-    // Keyboard events
-  
-    $window.keydown(function (event) {
-      // Auto-focus the current input when a key is typed
-      if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-        $currentInput.focus();
-      }
-      // When the client hits ENTER on their keyboard
-      if (event.which === 13) {
-        if (username) {
-          sendMessage();
-          socket.emit('stop typing');
-          typing = false;
-        } else {
-          setUsername();
-        }
-      }
+    );
+    setProgressBar(++current);
+  }
+  $(".next").click(function () {
+    var form = $("#msform");
+    form.validate({
+      highlight: function (element) {
+        console.log("error");
+        $(element).closest(".form-card").addClass("has-error");
+      },
+      unhighlight: function (element) {
+        console.log("nonerror");
+        $(element).closest(".form-card").removeClass("has-error");
+      },
+      rules: {
+        title: {
+          required: true,
+          minlength: 6,
+        },
+        url: {
+          required: true,
+        },
+      },
+      messages: {
+        title: {
+          required: "Title required",
+        },
+        url: {
+          required: "URL required",
+        },
+      },
     });
-  
-    $inputMessage.on('input', function() {
-      updateTyping();
-    });
-  
-    // Click events
-  
-    // Focus input when clicking anywhere on login page
-    $loginPage.click(function () {
-      $currentInput.focus();
-    });
-  
-    // Focus input when clicking on the message input's border
-    $inputMessage.click(function () {
-      $inputMessage.focus();
-    });
-  
-    // Socket events
-  
-    // Whenever the server emits 'login', log the login message
-    socket.on('login', function (data) {
-      connected = true;
-      // Display the welcome message
-      var message = "Welcome to Socket.IO Chat – ";
-      log(message, {
-        prepend: true
-      });
-      addParticipantsMessage(data);
-    });
-  
-    // Whenever the server emits 'new message', update the chat body
-    socket.on('new message', function (data) {
-      addChatMessage(data);
-    });
-  
-    // Whenever the server emits 'user joined', log it in the chat body
-    socket.on('user joined', function (data) {
-      log(data.username + ' joined');
-      addParticipantsMessage(data);
-    });
-  
-    // Whenever the server emits 'user left', log it in the chat body
-    socket.on('user left', function (data) {
-      log(data.username + ' left');
-      addParticipantsMessage(data);
-      removeChatTyping(data);
-    });
-  
-    // Whenever the server emits 'typing', show the typing message
-    socket.on('typing', function (data) {
-      addChatTyping(data);
-    });
-  
-    // Whenever the server emits 'stop typing', kill the typing message
-    socket.on('stop typing', function (data) {
-      removeChatTyping(data);
-    });
+    if (form.valid() === true) {
+      if ($(".title-section").is(":visible")) {
+        current_fs = $(".title-section");
+        next_fs = $(".url-section");
+        progessBarFunction();
+      } else if ($(".url-section").is(":visible")) {
+        current_fs = $(".url-section");
+        next_fs = $(".confirm-section");
+        progessBarFunction();
+      }
+      next_fs.show();
+      current_fs.hide();
+    }
   });
+  $(".previous").click(function () {
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+    $("#progressbar li")
+      .eq($("fieldset").index(current_fs))
+      .removeClass("active");
+    previous_fs.show();
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now) {
+          opacity = 1 - now;
+          current_fs.css({
+            display: "none",
+            position: "relative",
+          });
+          previous_fs.css({ opacity: opacity });
+        },
+        duration: 500,
+      }
+    );
+    setProgressBar(--current);
+  });
+  function setProgressBar(curStep) {
+    var percent = parseFloat(100 / steps) * curStep;
+    percent = percent.toFixed();
+    $(".progress-bar").css("width", percent + "%");
+  }
+});
+
+document
+  .getElementsByClassName("sureButton")[0]
+  .addEventListener("click", () => {
+    document.getElementsByClassName("urltitle")[0].innerHTML =
+      $(".getTitle").val();
+    document.getElementsByClassName("url")[0].innerHTML = $(".getURL").val();
+  });
+document.getElementById("msform").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
