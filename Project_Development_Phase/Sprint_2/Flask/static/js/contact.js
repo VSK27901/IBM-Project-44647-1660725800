129c0fd73f6f83b847ad9c891e915af628ec74fd
sreelakshmig009 <sreelakshmig009@gmail.com>
2022-10-29 21:58:01 +0530

    Tried to connect emailjs service

--- Project_Development_Phase/Sprint_2/Flask/static/js/contact.js
+++ Project_Development_Phase/Sprint_2/Flask/static/js/contact.js
@@ -1,3 +1,23 @@
 AOS.init({
   duration: 1200,
 });
+
+(function (){
+  emailjs.init("user_u91gztpRrID-R71OI");
+})();
+
+function sendEmail(e){
+  e.preventDefault();
+  let name = document.getElementsByClassName("name")[0].value;
+  let email = document.getElementsByClassName("email")[0].value;
+  let msg = document.getElementsByClassName("msg")[0].value;
+
+    var contactParams = {
+      fullname : name,
+      emailid : email,
+      message : msg
+      
+    };
+    console.log(contactParams);
+    emailjs.send('service_avzu5ub','template_t84mfbr',contactParams).then(function (res) {console.log(res);})
+}
